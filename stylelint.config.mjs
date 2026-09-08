88d49643d51834db29a52cb0cd3fdfdc17c68b9c
RJ <rybertjen@gmail.com>
2026-09-07 22:31:39 -0700

    ci(validation): focus CSS gate on structural errors

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('stylelint').Config} */
 export default {
   extends: ['stylelint-config-recommended'],
+  rules: {
+    'no-descending-specificity': null,
+    'declaration-property-value-keyword-no-deprecated': null,
+  },
 };
