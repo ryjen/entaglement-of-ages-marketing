995d63c72cec369fb87ce43e3a8fc097e74131e3
RJ <rybertjen@gmail.com>
2026-09-07 23:20:06 -0700

    test(ux): remove checksum diagnostic

--- tests/checksum-centering.test.mjs
+++
@@ -1,8 +0,0 @@
-import test from 'node:test';
-import crypto from 'node:crypto';
-import { readFileSync } from 'node:fs';
-
-test('print centered homepage checksum', () => {
-  const hash = crypto.createHash('sha256').update(readFileSync('src/index.html')).digest('hex');
-  console.log(`checksum src/index.html ${hash}`);
-});
